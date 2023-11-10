97ba52213f70893ea3da0c8a12dea952612a64d9
Surendra Jammishetti <suri312006@gmail.com>
2023-11-10 11:15:38 -0800

    redirect to signup page if user not signed in

--- src/routes/(home)/+page.ts
+++ src/routes/(home)/+page.ts
@@ -2,9 +2,10 @@ import { get_current_user, get_user_data } from "$lib/backend/db_get";
 import type { PageLoad } from "./$types";
 
 export const load = (async () => {
-  let user_data: any = await get_user_data();
+  let user: any = await get_current_user();
 
-  console.log(user_data);
+  // console.log(user);
 
-  return {};
+
+  return {user};
 }) satisfies PageLoad;
