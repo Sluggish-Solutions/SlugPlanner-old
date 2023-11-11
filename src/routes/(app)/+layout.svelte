69d47f2284670a2007bf1ab68bc1e4347d77de7d
Surendra Jammishetti <suri312006@gmail.com>
2023-11-10 17:37:30 -0800

    header + login / logout stuff fixed

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import "../../app.postcss";
+    import Header from '../Header.svelte'
+  </script>
+  
+  
+  <div id="container">
+    
+    <div id="header">
+
+      <Header />
+    </div>
+  
+    <div id="main-section">
+          <main>
+              <slot />
+              <!-- <Footer /> -->
+          </main>
+      </div>
+  
+  </div>
+  
