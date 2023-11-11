1ae687239339fece459eb84941c372149f09e663
aleclevy <aleclevy3@gmail.com>
2023-11-11 00:12:08 -0800

    class link + general layout progress

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,7 @@
+<!-- YourParentComponent.svelte -->
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import Quarter from '$lib/components/quarter.svelte';
+  import Quarter from '$lib/components/Quarter.svelte';
   import { onMount } from 'svelte';
 
   export let data;
@@ -12,7 +13,45 @@
   });
 </script>
 
+<style>
+  /* Ensure the body and HTML have 100% height */
+  html, body {
+    height: 100%;
+    margin: 0; /* Remove default margin */
+    padding: 0; /* Remove default padding */
+  }
 
-<div class="wrapper grid grid-cols-3 gap-4">
-  <Quarter />
+  /* Ensure the entire container has 100% height */
+  #app {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  /* Ensure the content area fills the remaining height */
+  .content {
+    flex-grow: 1;
+  }
+
+  /* Ensure the wrapper has 100% height */
+  .wrapper {
+    height: 100%;
+  }
+</style>
+<div class= "px-[14rem]">
+<div class="p-12 border border-cyan-400">
+  <div class="wrapper grid grid-cols-3 grid-rows-4 gap-2">
+    <Quarter quarter_year="lol" />
+    <Quarter class1="AM 10"/>
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter />
+    <Quarter />
+  </div>
+</div>
 </div>
