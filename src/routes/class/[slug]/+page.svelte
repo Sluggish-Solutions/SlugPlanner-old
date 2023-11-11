9bfb21513902cc8368c10ac10e0773671dc40c95
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2023-11-10 16:52:18 -0800

    class slug toggle feature

--- src/routes/class/[slug]/+page.svelte
+++ src/routes/class/[slug]/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types'
-	import * as Collapsible from '$lib/components/ui/collapsible'
-	import { ChevronsUpDown } from 'lucide-svelte'
-	import { Button } from '$lib/components/ui/button'
 
 	export let data: PageData
 
 	// check if course exists
-	// $: console.log(data)
+	$: console.log(data)
 
 	$: course = {
 		name: data.course[0].name,
@@ -67,7 +64,7 @@
 							{pre_req}
 						</a>
 					{:else}
-						MP {pre_req}
+						Math Placement {pre_req}
 					{/if}
 					{#if k != course.pre_reqs[i].length - 1}
 						or
@@ -94,32 +91,6 @@
 			: None
 		{/if}
 	</p>
-
-	<Collapsible.Root class="w-[300px] space-y-2">
-		<div class="flex items-center justify-between px-4">
-			<h4 class="text-sm font-semibold">Show More</h4>
-			<Collapsible.Trigger asChild let:builder>
-				<Button
-					builders={[builder]}
-					variant="ghost"
-					size="sm"
-					class="w-9 p-0"
-				>
-					<span class="flex">
-						<ChevronsUpDown class="h-4 w-8" />
-					</span>
-				</Button>
-			</Collapsible.Trigger>
-		</div>
-		<Collapsible.Content class="space-y-2">
-			<div class="rounded-md border px-4 py-3 font-mono text-sm">
-				bits-ui
-			</div>
-			<div class="rounded-md border px-4 py-3 font-mono text-sm">
-				@radix-ui/primitives
-			</div>
-		</Collapsible.Content>
-	</Collapsible.Root>
 </div>
 <!-- {#if data.course.pre_reqs}
 	<p>Next post: <a href="/blog/{next.slug}">{next.title}</a></p>
